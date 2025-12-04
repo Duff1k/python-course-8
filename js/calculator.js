d191fcddb4bee6c620579632531348fd2070431c
malkovaanya <nusha.04@bk.ru>
2025-12-05 00:33:03 +0300

    js

--- js/calculator.js
+++ js/calculator.js
@@ -1,21 +1,18 @@
-// Добавление значения на экран
 function appendToDisplay(value) {
     document.getElementById('result').value += value;
 }
 
-// Очистка экрана
+
 function clearDisplay() {
     document.getElementById('result').value = '';
 }
 
-// Вычисление результата
 function calculate() {
     try {
         const result = eval(document.getElementById('result').value);
         const expression = document.getElementById('result').value;
         document.getElementById('result').value = result;
 
-        // Добавление операции в историю
         const history = document.getElementById('history');
         history.innerHTML += `<p>${expression} = ${result}</p>`;
     } catch (error) {
@@ -23,7 +20,6 @@ function calculate() {
     }
 }
 
-// Очистка истории
 function clearHistory() {
     document.getElementById('history').innerHTML = '';
 }
